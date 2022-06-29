e028ad127d0ff6744046a69980f935ecfc3ed2a9
Антон <toshabespalov2006@gmail.com>
2022-06-29 20:56:13 +0300

    Anton's homework

---
+++ src/main/java/com/homework/CustomArrayList.java
@@ -0,0 +1,160 @@
+package com.homework;
+
+import java.util.*;
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+public class CustomArrayList<E> implements List<E> {
+
+    private int size;
+    private Object[] elementData;
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return indexOf(o) >= 0;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return Arrays.copyOf(elementData, size);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> T[] toArray(T[] a) {
+        //noinspection ConstantConditions
+        return null;
+    }
+
+    @Override
+    public boolean add(E e) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        final int indexOf = indexOf(o);
+        if (indexOf != -1) {
+            elementData[indexOf] = null;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        Arrays.fill(elementData, null);
+    }
+
+    @SuppressWarnings("unchecked")
+    E elementData(int index) {
+        return (E) elementData[index];
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        return elementData(index);
+    }
+
+    @Override
+    public E set(int index, E element) {
+        Objects.checkIndex(index, size);
+        E oldValue = elementData(index);
+        elementData[index] = element;
+        return oldValue;
+    }
+
+    @Override
+    public void add(int index, E element) {
+    }
+
+    @Override
+    public E remove(int index) {
+        final Object datum = elementData[index];
+        if (datum != null) {
+            elementData[index] = null;
+            return (E) datum;
+        }
+        return null;
+    }
+
+    int indexOfRange(Object o, int start, int end) {
+        for (int i = start; i < end; i++) {
+            if (o.equals(elementData[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return indexOfRange(o, 0, size);
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return lastIndexOfRange(o, 0, size);
+    }
+
+    int lastIndexOfRange(Object o, int start, int end) {
+        return start;
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+}
+
