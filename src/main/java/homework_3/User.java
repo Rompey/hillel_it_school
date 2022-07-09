4e608a8cc7dfd61d9329685e7705580f0137f824
Антон <toshabespalov2006@gmail.com>
2022-07-09 16:06:09 +0300

    Anton's homework 4

---
+++ src/main/java/homework_3/User.java
@@ -0,0 +1,30 @@
+package homework_3;
+
+import java.util.Objects;
+
+
+public class User {
+    private final int id;
+    private final String name;
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int hashCode = name.hashCode();
+        hashCode = 31 * hashCode + id;
+
+        return hashCode;
+    }
+}
